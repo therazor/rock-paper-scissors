2eca8e664c4b8f0225569ad66eb0555cc018d0b5
Dario De Bastiani <dario.debastiani@gmail.com>
2017-11-13 22:14:43 +0000

    expect paper to win vs rock

--- src/play/play.js
+++ src/play/play.js
@@ -1,5 +1,15 @@
 const Play = () => ({
-    winner: ''
+    winner: PLAYER.N1
 });
 
 export default Play;
+
+export const PLAYER = {
+    N1: Symbol('PLAYER_1'),
+    N2: Symbol('PLAYER_2'),
+};
+
+export const MOVE = {
+    ROCK: Symbol('ROCK'),
+    PAPER: Symbol('PAPER'),
+};
--- src/play/play.test.js
+++ src/play/play.test.js
@@ -1,4 +1,4 @@
-import play from './play';
+import { default as play, PLAYER, MOVE }  from './play';
 
 it('returns a game status when playing', () => {
   const status = play();
@@ -9,5 +9,16 @@ it('returns a winner when playing', () => {
   const status = play();
   expect(status).toMatchObject({
     winner: expect.anything()
+  });
+});
+
+it('identifies paper as winner vs rock', () => {
+  const status = play({
+    [PLAYER.N1]: MOVE.PAPER,
+    [PLAYER.N2]: MOVE.ROCK,
+  });
+
+  expect(status).toMatchObject({
+    winner: PLAYER.N1
   });
 });
