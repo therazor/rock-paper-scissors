bc3b904d72cf9c31507d311d9dbb5e2762084d69
Dario De Bastiani <dario.debastiani@gmail.com>
2017-11-14 22:58:54 +0000

    add visual layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="wrapper">
+        <div className="container">
+          <header>
+              <ul>
+                  <li><button className="pick-player-type computer">Computer</button></li>
+                  <li><button className="pick-player-type human" disabled>Human</button></li>
+              </ul>
+          </header>
+
+          <main role="main">
+              <div className="player player-1 computer">
+                  <div className="player-type">Computer</div>
+                  <div className="hand rock"></div>
+              </div>
+
+              <div className="player player-2">
+                  <div className="player-type human">You</div>
+                  <div className="player-type computer">Computer 2</div>
+                  <div className="hand rock"></div>
+              </div>
+          </main>
+        </div>
+
+        <ul className="action-picker">
+            <li><button className="pick-action ir rock">Rock</button></li>
+            <li><button className="pick-action ir paper">Paper</button></li>
+            <li><button className="pick-action ir scissors">Scissors</button></li>
+        </ul>
+
+        <div className="outcome">
+            <span></span>
+        </div>
       </div>
     );
   }
