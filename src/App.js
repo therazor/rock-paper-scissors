d1ba5da06417c5eeb3404c9170bdab2b1577f841
Dario De Bastiani <dario.debastiani@gmail.com>
2017-11-15 21:45:55 +0000

    correctly indent HTML

--- src/App.js
+++ src/App.js
@@ -7,34 +7,34 @@ class App extends Component {
       <div className="wrapper">
         <div className="container">
           <header>
-              <ul>
-                  <li><button className="pick-player-type computer">Computer</button></li>
-                  <li><button className="pick-player-type human" disabled>Human</button></li>
-              </ul>
+            <ul>
+              <li><button className="pick-player-type computer">Computer</button></li>
+              <li><button className="pick-player-type human" disabled>Human</button></li>
+            </ul>
           </header>
 
           <main role="main">
-              <div className="player player-1 computer">
-                  <div className="player-type">Computer</div>
-                  <div className="hand rock"></div>
-              </div>
+            <div className="player player-1 computer">
+              <div className="player-type">Computer</div>
+              <div className="hand rock"></div>
+            </div>
 
-              <div className="player player-2">
-                  <div className="player-type human">You</div>
-                  <div className="player-type computer">Computer 2</div>
-                  <div className="hand rock"></div>
-              </div>
+            <div className="player player-2">
+              <div className="player-type human">You</div>
+              <div className="player-type computer">Computer 2</div>
+              <div className="hand rock"></div>
+            </div>
           </main>
         </div>
 
         <ul className="action-picker">
-            <li><button className="pick-action ir rock">Rock</button></li>
-            <li><button className="pick-action ir paper">Paper</button></li>
-            <li><button className="pick-action ir scissors">Scissors</button></li>
+          <li><button className="pick-action ir rock">Rock</button></li>
+          <li><button className="pick-action ir paper">Paper</button></li>
+          <li><button className="pick-action ir scissors">Scissors</button></li>
         </ul>
 
         <div className="outcome">
-            <span></span>
+          <span></span>
         </div>
       </div>
     );
