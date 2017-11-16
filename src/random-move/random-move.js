9a6eab17ab1d364ac1a2bf3a0fd8470b61521549
Dario De Bastiani <dario.debastiani@gmail.com>
2017-11-16 12:33:19 +0000

    make random moves actually random

--- src/random-move/random-move.js
+++ src/random-move/random-move.js
@@ -1,5 +1,11 @@
 import { MOVE } from '../play/play'
 
-const randomMove = () => MOVE.PAPER;
+const moves = Object.values(MOVE);
+
+// should be refactored out, but used only here for now
+const randomNumber = (min = 0, max = 1) =>
+    Math.floor(Math.random() * (max - min + 1)) + min;
+
+const randomMove = () => moves[randomNumber(0, moves.length - 1)];
 
 export default randomMove;
